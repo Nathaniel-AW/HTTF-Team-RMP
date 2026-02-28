5ecae5e92cf295c4b99fecec15f983fd479fd4e5
Andrew Ngantung <drewngantung@gmail.com>
2026-02-28 15:58:06 -0800

    connect to supabase

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import SupabaseTest from './SupabaseTest.jsx'
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>HTTF Team RMP</h1>
+        <p>Supabase client configured; inspect the data below.</p>
+      </header>
+      <SupabaseTest />
+    </main>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDom from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import App from './App'
 import './index.css'
+<<<<<<< HEAD
+=======
+import App from './components/App.jsx'
+>>>>>>> b710310 (connect to supabase)
 
 ReactDom.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
